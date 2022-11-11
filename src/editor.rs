ef7316053681a4b709bd23247c1a6ee5685837b7
L-Andy <lutaayaandrew200@gmail.com>
2022-11-11 08:16:00 -0500

    Adding terminal mode

---
+++ src/editor.rs
@@ -0,0 +1,61 @@
+use std::io::{ self, stdout, Write };
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+        }
+    }
+    pub fn defualt() -> Self {
+        Self { should_quit: false }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Good bye!\r");
+        }
+        io::stdout().flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Self::read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => {
+                self.should_quit = true;
+            }
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
+    panic!("{}", e);
+}
